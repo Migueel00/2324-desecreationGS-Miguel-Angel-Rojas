d1257e8fcab57f6596656b98a3ede6318a04499c
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-06-05 10:15:48 +0200

    Read db data & Create Dies

--- index.mjs
+++ index.mjs
@@ -1,8 +1,12 @@
 import DieFactory from "./DieFactory.mjs";
 import { DieID } from "./constants.mjs";
 
+const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
 
-const DIE = DieFactory.createDie(DieID.D3);
-const roll = DIE.roll();
-console.log("Roll dado:  " + roll)
+const data = await response.json();
+
+const Die3      = DieFactory.createDie(DieID.D3);
+const Die5      = DieFactory.createDie(DieID.D5);
+const Die20     = DieFactory.createDie(DieID.D20);
+const Die100    = DieFactory.createDie(DieID.D100);
 
