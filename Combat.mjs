a4ce12ae3aa8a74a734717c0184f937fae5eb0c7
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-06-05 11:30:37 +0200

    assign first turn

--- Combat.mjs
+++ Combat.mjs
@@ -3,9 +3,28 @@ export default class Combat{
         this.superHero = superHero;
         this.villain   = villain;
         this.die       = die;
+        this.turns     = [];
     }
 
     execute(){
+        this.assignFirstTurn();
+
+        console.log("El primer asalto es para " + this.turns[0].name);
+
+    }
+
+    assignFirstTurn(){
+        const scoreHero     = this.superHero.intelligence + this.superHero.combat;
+        const scoreVillain  = this.villain.intelligence + this.villain.combat;
+
+        this.turns = scoreHero > scoreVillain ? [this.superHero, this.villain] : [this.villain, this.superHero];
+    }
+
+    attackSuccesfullOrFlumble(){
+
+    }
+
+    attackDamage(){
         
     }
 }
--- index.mjs
+++ index.mjs
@@ -1,20 +1,32 @@
 import DieFactory from "./DieFactory.mjs";
 import { DieID } from "./constants.mjs";
 import TraininGround from "./TrainingGround.mjs";
+import Combat from "./Combat.mjs";
 
 const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
 
 const data = await response.json();
 const trainingGround = new TraininGround(data);
 
-const SuperHero = trainingGround.createSuperHero();
+const superHero = trainingGround.createSuperHero();
 const villain   = trainingGround.createVillain();
 
-console.log("heroe: " + SuperHero.hp);
-console.log("villano: " + villain.hp);
 
 const Die3      = DieFactory.createDie(DieID.D3);
 const Die5      = DieFactory.createDie(DieID.D5);
 const Die20     = DieFactory.createDie(DieID.D20);
 const Die100    = DieFactory.createDie(DieID.D100);
 
+const ArrayOfDies = [Die3, Die5, Die20, Die100];
+
+const combat = new Combat(superHero, villain, ArrayOfDies);
+
+console.log("¡WELCOME TO THE COMBAT ARENA!");
+console.log("-----------------------------")
+console.log("¡Hoy combatiran " + superHero.name + " vs " + villain.name);
+console.log("-----------------------------")
+console.log("Listado de atributos");
+console.log(superHero);
+console.log(villain);
+console.log("-----------------------------")
+combat.execute();
