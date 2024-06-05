13fabc84a70d9685dba1aed4a558d0f47a2747be
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-06-05 12:14:28 +0200

    superHero & villain Attack function logic added

--- SuperHero.mjs
+++ SuperHero.mjs
@@ -1,4 +1,7 @@
 import Character from "./Character.mjs";
+import Die from "./Die.mjs";
+import DieFactory from "./DieFactory.mjs";
+import { DieID } from "./constants.mjs";
 
 export default class SuperHero extends Character{
     constructor(name, hp, intelligence, strength, durability, speed, power, combat) {
@@ -6,7 +9,72 @@ export default class SuperHero extends Character{
         super(name, hp, intelligence, strength, durability, speed, power, combat);
     }
 
-    attack(){
+    attack(dieResult){
+        if (dieResult >= 0 && dieResult <= 2) {
 
+            // fallo
+            return flumbleDamage = this.calculateFlumbleDamage(dieResult);
+
+        } else if (dieResult >= 3 && dieResult <= 17) {
+
+            // exitoso
+            return normalDamage = (this.power + this.strength) * dieResult/ 100;
+
+        } else {
+
+             // critico
+            const normalDamage = (this.power + this.strength) * dieResult/ 100;
+            const criticDamage = this.calculateCriticDamage(dieResult);
+
+            const totalCriticDamage = normalDamage + criticDamage;
+            return totalCriticDamage;
+        }
+    }
+
+    calculateCriticDamage(dieResult){
+        let die3 = DieFactory.createDie(DieID.D3);
+        let roll;
+        let damage;
+
+        switch(dieResult){
+            case 18:
+                roll    = die3.roll;
+                damage  = ((this.intelligence * this.durability)/ 100 ) * roll;
+                break;
+            
+            case 19:
+                roll    = die3.roll + die3.roll;
+                damage  = ((this.intelligence * this.durability)/ 100 ) * roll;
+                break;
+            
+            case 20:
+                let die5    = DieFactory.createDie(DieID.D5);
+                roll        = die5.roll + die5.roll + die5.roll;
+                damage      = ((this.intelligence * this.durability)/ 100 ) * roll;
+                break;
+            
+        }
+
+        return damage;
+    }
+    
+    calculateFlumbleDamage(dieResult){
+        let die3 = DieFactory.createDie(DieID.D3);
+        let roll;
+        let damage;
+
+        switch(dieResult){
+            case 1:
+                roll    = die3.roll;
+                damage  = this.speed / roll; 
+                break;
+
+            case 2:
+                roll    = die3.roll + die3.roll + die3.roll + die3.roll;
+                damage  = this.speed / roll;
+                break;
+        }
+
+        return damage;
     }
 }
--- Villain.mjs
+++ Villain.mjs
@@ -1,4 +1,6 @@
 import Character from "./Character.mjs";
+import DieFactory from "./DieFactory.mjs";
+import { DieID } from "./constants.mjs";
 
 export default class Villain extends Character{
     constructor(name, hp, intelligence, strength, durability, speed, power, combat) {
@@ -6,7 +8,49 @@ export default class Villain extends Character{
         super(name, hp, intelligence, strength, durability, speed, power, combat);
     }
 
-    attack(){
+    attack(dieResult){
+    
+        if (dieResult >= 1 && dieResult <= 17) {
 
+            // exitoso
+            return normalDamage = (this.power + this.strength) * dieResult/ 100;
+
+        } else {
+            
+             // critico
+            const normalDamage = (this.power + this.strength) * dieResult/ 100;
+            const criticDamage = this.calculateCriticDamage(dieResult);
+
+            const totalCriticDamage = normalDamage + criticDamage;
+            return totalCriticDamage;
+        }
     }
+
+    calculateCriticDamage(dieResult){
+        let die3 = DieFactory.createDie(DieID.D3);
+        let roll;
+        let damage;
+
+        switch(dieResult){
+            case 18:
+                roll    = die3.roll + die3.roll;
+                damage  = ((this.intelligence * this.durability)/ 100 ) * roll;
+                break;
+            
+            case 19:
+                roll    = die3.roll + die3.roll + die3.roll;
+                damage  = ((this.intelligence * this.durability)/ 100 ) * roll;
+                break;
+            
+            case 20:
+                let die5    = DieFactory.createDie(DieID.D5);
+                roll        = die5.roll + die5.roll + die5.roll + die5.roll;
+                damage  = ((this.intelligence * this.durability)/ 100 ) * roll;
+                break;
+            
+        }
+
+        return damage;
+    }
+    
 }
